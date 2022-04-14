8913b1d4f75fe58740c4847f0c751672b7b5cc41
LordofUniverse <63595155+LordofUniverse@users.noreply.github.com>
2022-04-14 12:10:19 +0600

    Add files via upload

--- src/cart.js
+++ src/cart.js
@@ -1,10 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 import './cart.css'
+import home from "./home.jpg";
 
 function Cart() {
 
-    return (
-        <div class = "y">
+    let navigate = useNavigate(); 
 
+    const ToHome = () =>{ 
+        navigate("../");
+    }
+
+    return (
+        <div class = "App3">
+            <div class = "symb2">
+                <img class = "symbimg2" src = {home} alt = "..." onClick={ToHome}/>
+            </div>
+            
+            <div class = "tit">
+                My Cart
+            </div>
+            <div class = "cont"></div>
         </div>
     )
 
